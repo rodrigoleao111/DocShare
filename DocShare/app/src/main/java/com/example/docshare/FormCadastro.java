ce53b74eff29af8f0bd6c2fd8a152a6b4383fcd2
Rodrigo Leão <rodrigoleao1995@gmail.com>
2022-07-13 11:17:36 -0300

    Correcao visibilidade de estilos e adicao de sombras na FormLogin

--- DocShare/app/src/main/java/com/example/docshare/FormCadastro.java
+++ DocShare/app/src/main/java/com/example/docshare/FormCadastro.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TesteDeLayout extends AppCompatActivity {
+public class FormCadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teste_de_layout);
+        setContentView(R.layout.activity_form_cadastro);
     }
 }
