357fe48a3bce8f3d45ee21392d679d29fcee6765
Dayv1dx <dayv1dxdev@gmail.com>
2022-07-20 11:31:17 -0300

    add tela de splash

---
+++ DocShare/app/src/main/java/com/example/docshare/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.docshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+    }
+
+}
