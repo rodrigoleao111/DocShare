ff6dd15433242afe7b4abcc1d8f5b168fb5c284b
Rodrigo Leão <rodrigoleao1995@gmail.com>
2022-07-20 16:02:19 -0300

    Metodo deslogar usuario

--- DocShare/app/src/main/java/com/example/docshare/MainActivity.java
+++ DocShare/app/src/main/java/com/example/docshare/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.docshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView bt_nova_os_manutencao, bt_historico_de_atividades, bt_configuracoes;
+    private TextView txt_nome_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        // Por enquanto vou usar com a função de deslogar
+        bt_configuracoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getApplicationContext(), "Usuário deslogado", Toast.LENGTH_LONG).show();
+                Intent back_to_login = new Intent(getApplicationContext(), FormLogin.class);
+                startActivity(back_to_login);
+            }
+        });
+
+
+    }
+
+
+    private void IniciarComponentes(){
+        bt_nova_os_manutencao = findViewById(R.id.image_bt_nova_os);
+        bt_historico_de_atividades = findViewById(R.id.image_historico);
+        bt_configuracoes = findViewById(R.id.image_configuracoes);
+
+        txt_nome_user = findViewById(R.id.txt_boas_vindas);
 
     }
 }
