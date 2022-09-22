d36a55381df2cbd89f2db490993645229a366fc3
Dayv1dx <dayv1dxdev@gmail.com>
2022-09-21 21:01:16 -0300

    atualizando o fragment histórico

--- DocShare/app/src/main/java/com/example/docshare/fragments/HistoricoFragment.java
+++ DocShare/app/src/main/java/com/example/docshare/fragments/HistoricoFragment.java
@@ -15,34 +15,12 @@ import com.example.docshare.R;
 
 public class HistoricoFragment extends Fragment {
 
-    private ListView lista;
-    private String[] itens = {"OS Exemplo 1","OS Exemplo 2","OS Exemplo 3","OS Exemplo 4","OS Exemplo 5",
-            "OS Exemplo 6","OS Exemplo 7","OS Exemplo 8","OS Exemplo 9","OS Exemplo 10","OS Exemplo 11",
-    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_historico, container, false);
-
-       lista = view.findViewById(R.id.listView);
-
-       //Criar adaptador para a lista
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, itens);
-
-        //Adiciona adaptador para a lista
-        lista.setAdapter(adapter);
-
-        //Adicionando clique na lista
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                String valorSelecionado = lista.getItemAtPosition(position).toString();
-            }
-        });
 
 
         return view;
